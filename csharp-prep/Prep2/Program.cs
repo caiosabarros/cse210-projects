33793790fc150f7ab2a0f6894395c5798a556112
Caio Sa <caiohenriquesa1@gmail.com>
2023-01-03 21:35:43 -0300

    printing in C#

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+        Console.WriteLine("What's your grade percentage?");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+    
+//A >= 90
+//B >= 80
+//C >= 70
+//D >= 60
+//F < 60
+
+    if (number >= 90 ) {
+        letter = "A";
+        Console.WriteLine("your grade percentage is B");
+    } else if (number >= 80) 
+    {
+        letter = "C";
+        Console.WriteLine("your grade percentage is C");
+    } else if (number >= 70) {
+        letter = "D";
+        Console.WriteLine("your grade percentage is D");
+    } else if (number >= 60) {
+        letter = "E";
+        Console.WriteLine("your grade percentage is E");
+    } else if (number < 60) {
+        letter = "F";
+        Console.WriteLine("your grade percentage is F");
+    } 
+
+    if(number < 70){
+        Console.WriteLine("Failed :(. Try next time");
+    } else {
+        Console.WriteLine("Congrats! You passed!");
+    }
+
     }
 }
