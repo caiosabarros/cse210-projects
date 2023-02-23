b348a0b0e54b8fe4317f32c20d1765f77a4d8dce
caiosabarros <caiohenriquesa1@gmail.com>
2023-02-22 21:34:20 -0300

    first step of develop04

---
+++ prove/Develop04/Menu.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Menu
+{
+    private string userInput = "";
+    private int intTemp = 0;
+    public int DisplayMenu()
+    {
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start Breathing Activity");
+        Console.WriteLine("2. Start Reflecting Activity");
+        Console.WriteLine("3. Start Listing Activity");
+        Console.WriteLine("4.Quit");
+        Console.WriteLine("Select a choice from the menu:");
+        intTemp = Convert.ToInt32(Console.ReadLine());
+        return intTemp;
+    }
+
+    public void DisplayStartingMessage(int intTemp)
+    {
+        if(intTemp == 1){
+        Console.WriteLine("This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing");
+        } else if(intTemp == 2)
+        {
+        Console.WriteLine(" This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+        } else if(intTemp== 3)
+        {
+        Console.WriteLine("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+        } else if(intTemp == 4){
+            Console.WriteLine("quitted");
+        }
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine();
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Menu menu = new Menu();
+        int selected =menu.DisplayMenu();
+        menu.DisplayStartingMessage(selected);
     }
 }
